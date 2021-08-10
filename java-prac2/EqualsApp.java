1facbca5cc780aecb4c33bbc20b8d20dbcb24b73
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-10 22:13:39 +0900

    8월 10일 ==VS Equals

---
+++ java-prac2/EqualsApp.java
@@ -0,0 +1,11 @@
+public class EqualsApp {
+    
+    public static void main(String[] args){
+        int p1 = 1;
+        int p2 = 1;
+
+        String o1 = "java";
+        String o2 = new String("java");
+
+    }
+}
