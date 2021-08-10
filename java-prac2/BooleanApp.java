4f8c50a35e7c3d7b2ffb47c53d37e78cb4a31afb
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-10 22:12:13 +0900

    8월 10일 Boolean Datatype

---
+++ java-prac2/BooleanApp.java
@@ -0,0 +1,16 @@
+public class BooleanApp{
+
+    public static void main(String[] args){
+
+        System.out.println("One");
+        System.out.println(1);
+        System.out.println(true);  // boolean 안에는 2개의 데이터 타입이 있다. true, false
+        System.out.println(false); // true와 false는 변수로 사용불가
+        // true와 false는 이미 사용이 예정되어 있는 것이여서 reserved word라고 부른다.
+
+        String foo = "Hello World";
+        System.out.println(foo.contains("World")); // 참과 거짓을 표현할때 쓰는 데이터 타입 true, false boolean 데이터 타입
+
+    }
+    
+}
