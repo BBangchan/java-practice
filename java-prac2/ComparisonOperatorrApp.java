0a33ff8fd79dc1a8eca23fc047062c43725f99b4
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-10 22:12:51 +0900

    8월 10일 비교연산자

---
+++ java-prac2/ComparisonOperatorrApp.java
@@ -0,0 +1,10 @@
+public class ComparisonOperatorrApp {
+    
+    public static void main(String[] args){
+        
+        System.out.println(1>1);
+        System.out.println(1 == 1);
+        System.out.println(1 < 1);
+        System.out.println(1 >= 1);
+    }
+}
