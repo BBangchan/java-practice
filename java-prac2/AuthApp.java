98f1b6365ec68ae8a3e0311973d94bc25842a645
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-10 22:11:40 +0900

    8월 10일 조건문 응용2

---
+++ java-prac2/AuthApp.java
@@ -0,0 +1,33 @@
+public class AuthApp {
+    
+    public static void main(String[] args){
+            // 코딩짤때는 한줄짜고 확인하고 그러한 작업이 필요함.
+        String id = "egoing";
+        String inputId = args[0];
+
+        String pass ="1111";
+        String Inputpass = args[1];
+
+    
+        System.out.println("Hi.");
+
+        //if(inputId == id){
+        // if(inputId.equals(id)){ //inputId.equals(id)가 반환하는 것은 boolean이다.
+        //     if(inputPass.equals(pass)){
+        //         System.out.println("Master!");
+        //     }
+        //     else{
+        //         System.out.println("Wrong passWord!");
+        //     }
+        // }
+        // else{
+        //     System.out.println("Who Are You??");
+        // }
+        if(inputId.equals(id) && Inputpass.equals(pass)){ 
+                System.out.println("Master!");
+            }
+        else{
+            System.out.println("Who Are You??");
+        }
+    }
+}
