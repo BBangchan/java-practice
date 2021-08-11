b607984e2b9f482669a99d9cda06e5aa0f6844d7
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-11 23:34:41 +0900

    8월 11일 생성자와 this

--- java-prac2/ProjectOOP/Print.java
+++ java-prac2/ProjectOOP/Print.java
@@ -5,13 +5,19 @@ package ProjectOOP;
  */
 public class Print {
     public String delimiter = "";
+    public Print(String delimiter) { 
+        //생성자는 class의 이름과 똑같은 method를 정의하면 된다.
+        // datatype을 선언하지도 않는다. 
+        this.delimiter = delimiter;
+        // this를 사용하면 우리가 생성한 instance를 가르키는 것.
+    }
     public void A(){
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("A");
         System.out.println("A");
     }
     public void B(){
-        System.out.println(delimiter);
+        System.out.println(this.delimiter);
         System.out.println("B");
         System.out.println("B");
     }
