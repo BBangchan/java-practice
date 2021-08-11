a6bbce441ceedb122424c738daeca34c90235753
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-11 22:15:45 +0900

    8월 11일 class강의코드

---
+++ java-prac2/ProjectOOP/MyOOP.java
@@ -0,0 +1,25 @@
+package ProjectOOP;
+
+/**
+ * MyOOP
+ */
+
+public class MyOOP {
+
+    public static void main(String[] args) {
+        //..
+        Print.delimiter = "----";
+        Print.A();
+        Print.A();
+        Print.B();
+        Print.B();
+       
+        Print.delimiter = "****";
+        Print.A();
+        Print.A();
+        Print.B();
+        Print.B();  
+      
+    }
+    
+}
