b59cbd142d39f66d9aba621803cb75eb66e16b5d
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-11 20:46:24 +0900

    8월 11일 java Method 강의 코드

---
+++ java-prac2/ProjectMethod/OutputMethod.java
@@ -0,0 +1,18 @@
+package ProjectMethod;
+public class OutputMethod{
+
+    public static String a(){
+        // 코드 작성.
+        return "a";
+    }
+    public static int one(){  // int로 선언하면 return 값이 int
+        // 코드 작성.
+        return 1;
+    }   
+    public static void main(String[] args){
+        System.out.println(a());
+        System.out.println(one());
+    }
+    
+    
+}
--- java-prac2/ProjectMethod/WhyMethod.java
+++ java-prac2/ProjectMethod/WhyMethod.java
@@ -1,4 +1,8 @@
 package ProjectMethod;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WhyMethod{
                                      // 매개변수, parameter
     public static void printTwoTimesA(String text, String delimiter){ // 같은 동작을 여러번 반복하는 것을 편하게 하기 위해서 method로 만들었다.
@@ -6,11 +10,24 @@ public class WhyMethod{
         System.out.println(text);
         System.out.println(text);
     }
-    public static void main(String[] args){ // main이라는 이름을 가진 method이다.
+  
+    public static String twoTimes(String text, String delimeter){
+        String out = "";
+        out  = out + delimeter + "\n";
+        out  = out + text + "\n";
+        out  = out + text + "\n";
+        return out;
+    }
+
+    public static void main(String[] args) throws IOException { // main이라는 이름을 가진 method이다.
                        //인자, argument
         printTwoTimesA("a","-");
-        printTwoTimesA("a","*");
+        System.out.println(twoTimes("a", "-"));
+        FileWriter fw = new FileWriter("out.txt");
+        fw.write(twoTimes("a", "*"));
+        fw.close();
         printTwoTimesA("a","&");
+        //Email.send("egoing@a.com", "two times a", twoTimes("a", "&"));
         printTwoTimesA("b","!");
 
     }
