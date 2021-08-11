922c66cc84d9082b0ac6106151b65a59d0a9f93e
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-11 17:48:21 +0900

    8월 11일 method 사용하는 이유

---
+++ java-prac2/ProjectMethod/FirstMethod.java
@@ -0,0 +1,10 @@
+package ProjectMethod;
+public class FirstMethod{
+
+    public static void main(String[] args){ // main이라는 이름을 가진 method이다.
+        
+        System.out.println("Hello Method");
+        System.out.println(Math.floor(1.1));
+
+    }
+}
