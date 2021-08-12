c22e473bda8294a166af90d9bfb41a9d60260c2e
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-12 22:44:23 +0900

    8월 12일 java 상속

---
+++ java-prac2/ProjectInheritance/InheritanceApp.java
@@ -0,0 +1,33 @@
+package ProjectInheritance;
+
+/**
+ * InheritanceApp
+ */
+class Cal{
+    int v1,v2;
+    Cal(int v1,int v2){
+        System.out.println("Cal init!!");
+        this.v1 =v1; this.v2= v2;
+    }
+    public int sum(){
+        return this.v1+v2;
+    }
+}
+class Cal3 extends Cal{ //상속받은 class에 생성자가 있다면 반드시 부모class를 호출해주어야 한다.
+
+    Cal3(int v1, int v2){
+        super(v1, v2);
+        System.out.println("Cal3 init!!");
+    }
+    public int minus(){
+        return this.v1-v2;
+    }
+}
+public class InheritanceApp {
+    public static void main(String[] args) {
+        Cal c = new Cal(2,1);
+        Cal3 c3 = new Cal3(2,1);
+        System.out.println(c3.sum());
+        System.out.println(c3.minus());
+    }
+}
